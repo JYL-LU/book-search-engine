9e817c4d2662e0b0839109094a7a68cb9ffad455
Jane Lu <jyl.janelu@gmail.com>
2021-10-19 23:06:38 -0700

    server client

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,13 +2,12 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Book {
-    _id: ID!
-    bookId: String
     authors: [String]
     description: String
-    title: String
+    bookId: String!
     image: String
     link: String
+    title: String!
   }
 
   type User {
